1edb36eb25341455a4ccd40a218721063468b9c9
Mateusz Hajduczek <mateuszhajduczek17@gmail.com>
2021-10-27 12:57:38 +0100

    Delete DisplayProduct.php

--- DisplayProduct.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-include_once 'Config.php';
-
-$sql = "SELECT p.*, pdi.img FROM Products p INNER JOIN ProductImages pdi ON pdi.product_id = p.id WHERE pdi.is_featured = 1";
-$handle = 
-$handle->execute();
-$getAllProducts = $handle->fetchAll(PDO::FETCH_ASSOC);
-return $getAllProducts;
